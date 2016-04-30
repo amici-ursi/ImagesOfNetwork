7eee11f317fbe7bc9987508c7258355e9010edcf
Nathan Scowcroft <ns@endrake.com>
2016-04-30 10:46:31 -0400

    Add oauth setup script
    Updated readme to reflect.

--- images_of/__init__.py
+++ images_of/__init__.py
@@ -1,3 +1,32 @@
+# that's all of em. I'm sure we can trim it down a bit
+OAUTH_SCOPE = [
+        'account',
+        'creddits',
+        'edit',
+        'flair',
+        'history',
+        'identity',
+        'livemanage',
+        'modconfig',
+        'modcontributors',
+        'modflair',
+        'modlog',
+        'modothers',
+        'modposts',
+        'modself',
+        'modwiki',
+        'mysubreddits',
+        'privatemessages',
+        'read',
+        'report',
+        'save',
+        'submit',
+        'subscribe',
+        'vote',
+        'wikiedit',
+        'wikiread',
+]
+
 try:
     from . import local_settings as settings
 except ImportError:
