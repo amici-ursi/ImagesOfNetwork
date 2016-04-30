7b7ec62acb1dfe7ccb2aee99c519ac03d179c8e6
Nathan Scowcroft <ns@endrake.com>
2016-04-30 07:49:11 -0400

    Fix logging and use oauth for expand.py

--- images_of/entrypoints/expand.py
+++ images_of/entrypoints/expand.py
@@ -10,22 +10,30 @@ from images_of import settings, Reddit
 
 def create_sub(r, sub):
     try:
+        logging.info('Attempting to create /r/{}'.format(sub))
         r.create_subreddit(sub, sub)
+        logging.info('Created /r/{}'.format(sub))
     except SubredditExists:
         logging.warning('/r/{} exists'.format(sub))
     except:
-        traceback.print_exc()
+        logging.warning(traceback.format_exc())
 
 
 def invite_mods(r, sub):
     cur_mods = [u.name for u in r.get_moderators(sub)]
+    logging.debug('current mods for /r/{}: {}'.format(sub, cur_mods))
+
     need_mods = [mod for mod in settings.DEFAULT_MODS if mod not in cur_mods]
+    logging.debug('inviting {}'.format(need_mods))
+
     if not need_mods:
         return
 
     s = r.get_subreddit(sub)
     for mod in need_mods:
         s.add_moderator(mod)
+
+    logging.info('invited {} to moderate')
 
 
 def setup_notifications(r, sub):
@@ -37,9 +45,10 @@ def setup_notifications(r, sub):
         "filter-subreddits": []
         }""")
 
+    logging.info('Requesting notifications about /r/{} from /u/Sub_Mentions'
+                 .format(sub))
     r.send_message('Sub_Mentions', 'Action: Subscribe',
                    setup.replace('{{subreddit}}', sub))
-
 
 def setup_sub(r, sub, sub_settings, multi, skip_creation,
            skip_mods, skip_notifications):
@@ -48,12 +57,15 @@ def setup_sub(r, sub, sub_settings, multi, skip_creation,
         create_sub(r, sub)
 
     if sub_settings:
+        logging.info('Copying settings to /r/{}'.format(sub))
         r.set_settings(sub, **sub_settings)
 
     if not skip_mods:
         invite_mods(r, sub)
 
     if multi:
+        logging.info('Adding /r/{} to /user/{}/m/{}'
+                     .format(sub, settings.USERNAME, multi))
         multi.add_subreddit(sub)
 
     if not skip_notifications:
@@ -72,11 +84,13 @@ def main(subs, skip_multi, skip_settings, **kwargs):
 
     r = Reddit('Expand {} Network /u/{}'
                .format(settings.NETWORK_NAME, settings.USERNAME))
-    r.login()
+    r.oauth()
 
     sub_settings = None
     if not skip_settings:
         sub_settings = r.get_settings(settings.MASTER_SUB)
+    logging.debug('Copied information from {}: {}'
+                  .format(settings.MASTER_SUB, sub_settings)
 
     multi = None
     if not skip_multi and settings.MULTIREDDIT:
