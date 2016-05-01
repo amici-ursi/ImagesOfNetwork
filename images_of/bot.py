70cb63dd959de717b678cd76f468b1d7028b8cc2
Nathan Scowcroft <ns@endrake.com>
2016-05-01 17:13:42 -0400

    Start reworking the bot.

--- images_of/bot.py
+++ images_of/bot.py
@@ -1,6 +1,93 @@
+import re
+import datetime
+
+from praw.helpers import submission_stream
+from praw.errors import AlreadySubmitted, APIException
+
 class Bot:
     def __init__(self, r):
         self.r = r
 
+        self.blacklist_users = self._read_blacklist('userblacklist')
+        self.blacklist_subs = self._read_blacklist('subredditblacklist')
+
+        self.subreddits = []
+        for sub_settings in settings.SLAVE_SUBS:
+            sub = Subreddit(**slave_settings)
+            self.subreddits.append(sub)
+
+        ext_pattern = '({})$'.format('|'.join(settings.EXTENSIONS))
+        self.ext_re = re.compile(ext_pattern, flags=re.IGNORECASE)
+
+        domain_pattern = '^({})$'.format('|'.join(settings.DOMAINS))
+        self.domain_re = re.compile(domain_pattern, flags=re.IGNORECASE)
+
+    def _read_blacklist(self, wiki_page):
+        content = r.get_wiki_page(settings.MASTER_SUB, wiki_page).content_md
+        entries = [line.srip().lower()[3:] for line in content.splitlines() if line]
+        return set(entries)
+
+    def check(self, post):
+        """Check global conditions on a post"""
+        if post.over_18:
+            return False
+
+        user = post.author.name.lower()
+        if user in self.blacklist_users:
+            return False
+
+        sub = post.subreddit.display_name.lower()
+        if sub in self.blacklist_subs:
+            return False
+
+        if self.domain_re.search(post.domain):
+            return True
+
+        if self.ext_re.search(post.url):
+            return True
+
+        return False
+
+
+    def crosspost(self, post, sub):
+        title = post.title
+        comment = '[Original post]({}) by /u/{} in /r/{}'.format(
+                post.permalink,
+                post.author,
+                post.subreddit)
+
+        logging.info('X-Posting into /r/{}: {}'.format(sub.name, title))
+        try:
+            xpost = r.submit(
+                        sub.name
+                        url=post.url,
+                        captcha=None,
+                        send_replies=True,
+                        resubmit=False)
+            xpost.add_comment(comment)
+        except AlreadySubmitted:
+            logging.info('Already submitted. Skipping.')
+        except APIException as e:
+            logging.warning(e)
+        
+    def check_age(self, post):
+        created = datetime.utcfromtimestamp(post.author.created_utc)
+        age = (datetime.utcnow() - created).days
+        return age > 2
+
     def run(self):
-        pass
+        stream = submission_stream(self.r, 'all')
+        for post in stream:
+            if not self.check(post):
+                continue
+
+            age_ok = None
+            for sub in self.subreddits
+                if sub.check(post):
+                    if age_ok is None:
+                        age_ok = self.check_age()
+                    
+                    if not age_ok:
+                        break
+
+                    self.crosspost(post, sub)
--- images_of/settings.py
+++ images_of/settings.py
@@ -26,3 +26,56 @@ MASTER_SUB = "ImagesOfNetwork"
 DEFAULT_MODS = ['AutoModerator', 'BotWatchman']
 DEFAULT_DESCRIPTION = 'An __ImagesOf__ Network Subreddit'
 WIKI_PAGES = ['config/automoderator', 'toolbox']
+
+# Post Checking
+
+EXTENSIONS = [
+    r'\.jpg',
+    r'\.jpeg',
+    r'\.png',
+    r'\.gif',
+    r'\.gifv',
+    r'\.apng',
+    r'\.tiff',
+    r'\.bmp',
+    r'\.xcf',
+]
+
+DOMAINS = [
+    r'500px\.com',
+    r'abload\.de',
+    r'cdn\.theatlantic\.com',
+    r'.*\.deviantart\.com',
+    r'.*\.deviantart\.net',
+    r'fav\.me',
+    r'.*\.fbcdn\.net',
+    r'.*\.files\.wordpress\.com',
+    r'flic\.kr',
+    r'flickr\.com',
+    r'forgifs\.com',
+    r'gfycat\.com',
+    r'(.*\.)?gifsoup\.com',
+    r'(.*\.)?gyazo\.com',
+    r'(.*\.)?imageshack\.us',
+    r'imgclean\.com',
+    r'(i\.)?imgur\.com',
+    r'instagr\.am',
+    r'instagram\.com',
+    r'(cdn\.)?mediacru\.sh',
+    r'(.*\.)?media\.tumblr\.com',
+    r'(.*\.)?min\.us',
+    r'(.*\.)?minus\.com',
+    r'(.*\.)?panoramio\.com',
+    r'photoburst\.net',
+    r'(.*\.)?photoshelter\.com',
+    r'pbs\.twimg\.com',
+    r'(.*\.)?photobucket\.com',
+    r'picsarus\.com',
+    r'puu\.sh',
+    r'scontent\.cdninstagram\.com',
+    r'(.*\.)?staticflickr\.com',
+    r'(.*\.)?tinypic\.com',
+    r'twitpic\.com',
+    r'upload.wikimedia\.org',
+    r'i\.reddituploads.com',
+]
