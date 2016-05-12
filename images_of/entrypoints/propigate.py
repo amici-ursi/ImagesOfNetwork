809e84134f5372ae788b6bbaa554849d51205218
Nathan Scowcroft <ns@endrake.com>
2016-05-12 19:35:51 -0400

    Update wiki page tags and make case insensitive

--- images_of/entrypoints/propigate.py
+++ images_of/entrypoints/propigate.py
@@ -7,14 +7,21 @@ from images_of.subreddit import Subreddit
 
 LOG = logging.getLogger(__name__)
 
-def split_content(content, start_delim, end_delim, tags_required=True):
+def split_content(content, start_delim, end_delim, tags_required=True, case_insensitive=False):
+    if case_insensitive:
+        content_match = content.lower()
+        start_delim = start_delim.lower()
+        end_delim = end_delim.lower()
+    else:
+        content_match = content
+
     if tags_required:
         ok = True
-        if start_delim not in content:
+        if start_delim not in content_match:
             LOG.warning('Missing {}'.format(start_delim))
             ok = False
 
-        if end_delim not in content:
+        if end_delim not in content_match:
             LOG.warning('Missing {}'.format(start_delim))
             ok = False
 
@@ -22,23 +29,28 @@ def split_content(content, start_delim, end_delim, tags_required=True):
             return None
 
     head = ""
-    if start_delim in content:
-        head, content = content.split(start_delim)
+    if start_delim in content_match:
+        start = content_match.find(start_delim)
+        end = start + len(start_delim)
+        head, content = content[:start], content[end:]
+        content_match = content_match[end:]
 
     tail = ""
-    if end_delim in content:
-        content, tail = content.split(end_delim)
+    if end_delim in content_match:
+        start = content_match.find(end_delim)
+        end = start + len(end_delim)
+        content, tail = content[:start] + content[end:]
 
     return (head, content, tail)
 
 def copy_wiki_page(r, page, dom, subs, force):
-    start_delim = "#Start-{}-Network-Config".format(settings.NETWORK_NAME)
-    end_delim = "#End-{}-Network-Config".format(settings.NETWORK_NAME)
+    start_delim = "#Start-{}-Network".format(settings.NETWORK_NAME)
+    end_delim = "#End-{}-Network".format(settings.NETWORK_NAME)
 
     content = r.get_wiki_page(dom, page).content_md
     # Throw away the head and tail sections, don't care about them, we won't
     # be copying them or editing this page.
-    content = split_content(content, start_delim, end_delim, False)[1]
+    content = split_content(content, start_delim, end_delim, False, True)[1]
 
     for sub in subs:
         sub_content = r.get_wiki_page(sub, page).content_md
