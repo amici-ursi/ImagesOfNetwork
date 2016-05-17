d1125a4b1a73277f783225bdf2c04a722332bc91
J. Smith <SmithsonianDSP@users.noreply.github.com>
2016-05-17 18:19:32 -0500

    Fix entry point for blacklist_requests

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
             "ion_invite_mods = images_of.entrypoints.invite_mods:main",
             "ion_bulkmail = images_of.entrypoints.bulkmail:main",
             "ion_audit_mods = images_of.entrypoints.audit_mods:main",
-            "ion_blacklist_requests = images_of.entrypoints.blacklist_requests.py:main",
+            "ion_blacklist_requests = images_of.entrypoints.blacklist_requests:main",
         ],
     },
 
