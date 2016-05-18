c161951a7b925f6d8ccf8bec4251ca8a16b08eb1
Joseph Smith <joseph.w.smith@gmail.com>
2016-05-18 10:26:29 -0500

    blacklist_requests only replies AFTER user is added to blacklist & will reply if user is already on blacklist

--- images_of/entrypoints/blacklist_requests.py
+++ images_of/entrypoints/blacklist_requests.py
@@ -12,12 +12,13 @@ def main():
     Check for blacklist requests and add users to blacklist.
     """
 
-    r = Reddit('{} Update User-Requested Blacklist v0.1 /u/{}'
+    r = Reddit('{} Update User-Requested Blacklist v0.2 /u/{}'
                .format(settings.NETWORK_NAME, settings.USERNAME))
     r.oauth()
 
     add_users = set()
     orig_blacklist = set()
+    requests = list()
 
     sub = settings.MASTER_SUB
 
@@ -33,15 +34,23 @@ def main():
             author = m.author.name.lower()
             if '/u/{}'.format(author) not in orig_blacklist:
                 add_users.add(author)
-                m.reply('Your images will no longer be crossposted on the {} Network.'
-                        .format(settings.NETWORK_NAME))
+                requests.append(m)
+
             else:
+                m.reply('It appears that you are already on the {} Network blacklist. If your images are still being crossposted, please let us know so we can investigate.'
+                        .format(settings.NETWORK_NAME))
                 LOG.info('User {} is already in blacklist; skipping'.format(m.author.name))
 
     if add_users:
-        update_user_blacklist(r, add_users, orig_blacklist)
+
+        if update_user_blacklist(r, add_users, orig_blacklist):
+            for m in requests:
+                m.reply('You have been added to the user blacklist and your images will no longer be crossposted on the {} Network.'
+                        .format(settings.NETWORK_NAME))
+
     else:
         LOG.info('No new blacklist requests to process')
+
 
 
 def get_user_blacklist(r):
@@ -73,6 +82,7 @@ def update_user_blacklist(r, add_users, orig_blacklist):
 
     r.edit_wiki_page(dom, page, new_content, 'Blacklist requests for: {}'.format(add_users))
 
+    return True
 
 if __name__ == '__main__':
     main()
