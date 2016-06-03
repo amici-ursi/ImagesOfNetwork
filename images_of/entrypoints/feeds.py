18a2fc9c17b477c3c63f85a2776b9e64ddda9b93
Nathan Scowcroft <ns@endrake.com>
2016-06-03 02:49:44 -0400

    use a Subreddit object

--- images_of/entrypoints/feeds.py
+++ images_of/entrypoints/feeds.py
@@ -4,6 +4,7 @@ import feedparser
 from praw.errors import AlreadySubmitted, APIException, HTTPException
 
 from images_of import command, settings, Reddit
+from imagesof.subreddit import Subreddit
 
 
 LOG = logging.getLogger(__name__)
@@ -14,15 +15,16 @@ def main():
     r = Reddit('posts relevant feeds to the {} - /u/{}'.format(settings.NETWORK_NAME, settings.USERNAME))
     r.oauth()
 
-    for child in settings.CHILD_SUBS:
-        for feed in child.feeds:
-            sub = child['name']
+    for child_settings in settings.CHILD_SUBS:
+        sub = Subreddit(**child_settings)
+
+        for feed in sub.feeds:
             thisfeed = feedparser.parse(feed)
 
             for item in thisfeed.entries:
-                LOG.info('Posting OC into /r/{}: {}'.format(sub, item.title))
+                LOG.info('Posting OC into /r/{}: {}'.format(sub.name, item.title))
                 try:
-                    r.submit(sub, title=item.title, url=item.link, captcha=None, send_replies=True, resubmit=False)
+                    r.submit(sub.name, title=item.title, url=item.link, captcha=None, send_replies=True, resubmit=False)
                 except AlreadySubmitted:
                     LOG.info('Already submitted. Skipping.')
                 except APIException as e:
