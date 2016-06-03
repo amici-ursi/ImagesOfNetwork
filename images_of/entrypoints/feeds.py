5305ea2a0e2da0b8c6ecaedbe6bfd7f446a388aa
amici <amici-ursi@users.noreply.github.com>
2016-06-02 23:23:51 -0500

    removed old prints

--- images_of/entrypoints/feeds.py
+++ images_of/entrypoints/feeds.py
@@ -10,7 +10,6 @@ def main():
     r = Reddit('posts relevant feeds to the {} - /u/{}'.format(settings.NETWORK_NAME, settings.USERNAME))
     r.oauth()
     children = settings.CHILD_SUBS
-#    print(children)
     for child in children:
         checkforfeeds = child.get('feeds', None)
         if checkforfeeds: 
@@ -18,7 +17,6 @@ def main():
                 thisfeed = feedparser.parse(feed)
                 for i in range(len(thisfeed.entries)):
                     LOG.info('Posting OC into /r/{}: {}'.format(child['name'], thisfeed.entries[i].title))
-#                    print("posting {}".format(thisfeed.entries[i].title))
                     try:
                         r.submit(child['name'], title=thisfeed.entries[i].title, url=thisfeed.entries[i].link, captcha=None, send_replies=True, resubmit=False)
                     except AlreadySubmitted:
