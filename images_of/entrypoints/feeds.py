65c478adfa08b181f78b4657c6d083747f1a618d
amici-ursi <amici-ursi@users.noreply.github.com>
2016-06-04 17:26:23 -0500

    tries to add a comment for context

--- images_of/entrypoints/feeds.py
+++ images_of/entrypoints/feeds.py
@@ -12,19 +12,31 @@ LOG = logging.getLogger(__name__)
 
 @command
 def main():
-    r = Reddit('posts relevant feeds to the {} - /u/{}'.format(settings.NETWORK_NAME, settings.USERNAME))
+    r = Reddit('posts relevant feeds to the {} - /u/{}'.format(
+        settings.NETWORK_NAME,
+        settings.USERNAME))
     r.oauth()
 
     for child_settings in settings.CHILD_SUBS:
         sub = Subreddit(**child_settings)
-
         for feed in sub.feeds:
             thisfeed = feedparser.parse(feed)
-
+            comment = '[This content brought to you from {}{}'.format(
+                    thisfeed.feed.title,
+                    COMMENT_FOOTER)
             for item in thisfeed.entries:
-                LOG.info('Posting OC into /r/{}: {}'.format(sub.name, item.title))
+                LOG.info('Posting OC into /r/{}: {}'.format(
+                    sub.name,
+                    item.title))
                 try:
-                    r.submit(sub.name, title=item.title, url=item.link, captcha=None, send_replies=True, resubmit=False)
+                    xpost = r.submit(
+                        sub.name,
+                        title=item.title,
+                        url=item.link,
+                        captcha=None,
+                        send_replies=True,
+                        resubmit=False)
+                    xpost.add_comment(comment)
                 except AlreadySubmitted:
                     LOG.info('Already submitted. Skipping.')
                 except APIException as e:
