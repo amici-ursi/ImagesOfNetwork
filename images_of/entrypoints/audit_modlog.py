c276743f570505120de240c684ccddb7eeb4f737
13steinj <13steinj@users.noreply.github.com>
2016-06-19 00:06:21 -0400

    Logical improvements to save resources
    - `while x: if y: x = not x; break`: the `x = not x` is redundant
    - while loops are redundant if we let PRAW do all the work while python's C `enumerate` counts on the spot for us.
    - `defaultdict.get(key, 0)` is redundant
    - Use dict literal instead of `dict` instantiation
    - `else` is useless if check leads to `continue`
    - Using the dummy date isn't needed and if anything is confusing to those that haven't read the code, use "Out of range" string instead
    - UAString version bump
    - docstring update

--- images_of/entrypoints/audit_modlog.py
+++ images_of/entrypoints/audit_modlog.py
@@ -1,17 +1,21 @@
 import click
 from collections import defaultdict
-from datetime import date, datetime, timedelta
+from datetime import datetime, timedelta
 from images_of import command, settings, Reddit
 
 @command
 @click.option('--history-days', help='How many days back into the modlog run. Default is 60 days', default=60)
 def main(history_days):
-    """Process modlogs to identify inactive mods"""
+    """Process modlogs to identify inactive mods.
+
+    Max 5k ModActions per modlog processed.
+
+    :param history_days: max days to check the modlog
+    """
     
     exclude_mods = settings.DEFAULT_MODS
-    dummy_min_date = date(1901, 1, 1)
 
-    r = Reddit('{} ModLog Auditor v0.1 - /u/{}'.format(settings.NETWORK_NAME, settings.USERNAME))
+    r = Reddit('{} ModLog Auditor v0.2 - /u/{}'.format(settings.NETWORK_NAME, settings.USERNAME))
     r.oauth()
 
     subs = sorted([sub['name'] for sub in settings.CHILD_SUBS])
@@ -20,58 +24,36 @@ def main(history_days):
     
     for sub in subs:
         print('Processing {} modlog...'.format(sub))
-        done = False
-        last = None
-        last_time = -1
-        count = 0
-        first_id = None
         
         s = r.get_subreddit(sub)
         real_mods = [u.name for u in s.get_moderators()
                      if u.name not in exclude_mods]
 
-        if not real_mods:   ## If no real moderators, don't process this subreddit modlog
+        if not real_mods:  # If no real moderators, don't process this subreddit modlog
             continue
-        else:
-            mod_action_count = defaultdict(lambda: 0)
-            mod_last_action = dict()
+        mod_action_count = defaultdict(lambda: 0)
+        mod_last_action = {}
+        log = enumerate(s.get_mod_log(limit=None), start=1)
+        for count, log_entry in log:
+            if l.mod not in real_mods:
+                continue
 
-            while not done:
-                log = list(s.get_mod_log(limit=500, params={"after": last}))
-
-                if len(log) == 0:
-                    done = True
-                    break
-                else:
-                    last = log[-1].id
-                
-                for log_entry in log:
-                    if l.mod not in real_mods:
-                        continue
-
-                    date_utc = datetime.utcfromtimestamp(log_entry.created_utc).date()
-
-                    if date_utc >= end_date:
-                        mod_action_count[log_entry.mod] += 1
-                        # set if not set, else ignore since actions are chronological
-                        mod_last_action.setdefault(log_entry.mod, date_utc)
-                            
-                    # Reached end date
-                    else:
-                        done = True
-                        break
-                    
-
-                if count >= 5000:
-                    done = True
-                    break
-                    
-                count += 500
+            date_utc = datetime.utcfromtimestamp(log_entry.created_utc).date()
+            if date_utc >= end_date:
+                mod_action_count[log_entry.mod] += 1
+                # set if not set, else ignore since actions are chronological
+                mod_last_action.setdefault(log_entry.mod, date_utc)
+            # Reached end date
+            else:
+                break
+            sub_count = count  # Used in intermediate sub-based stats
+            if count >= 5000:
+                break
 
 
-        print('Subredit Moderation Log Stats for {}:\t\t({} entries parsed)'.format(sub, count))
+        print('Subredit Moderation Log Stats for {}:\t\t({} entries parsed)'.format(sub, sub_count))
         for m in real_mods:
-            print('    {}:\n\tActions: {}  \tLast Active: {}'.format(m, mod_action_count.get(m, 0), mod_last_action.get(m, dummy_min_date)))
+            print('    {}:\n\tActions: {}  \tLast Active: {}'.format(m, mod_action_count[m], mod_last_action.get(m, "Out of range")))
 
 
 if __name__ == '__main__':
