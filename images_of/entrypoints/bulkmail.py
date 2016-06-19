937f91aa937860fe597a72eb4e280bfabf3152cc
13steinj <13steinj@users.noreply.github.com>
2016-06-19 00:17:08 -0400

    Fix typo
    messgae => message

--- images_of/entrypoints/bulkmail.py
+++ images_of/entrypoints/bulkmail.py
@@ -6,14 +6,11 @@ from images_of import command, settings, Reddit
 @click.option('-s', '--subject', required=True, help='Message subject')
 @click.option('-m', '--message', required=True, help='Message body')
 def main(subject, message):
-    ok = True
     if not subject:
         print('Subject may not be empty')
-        ok = False
     if not message:
-        print('Messgae may not be empty')
-        ok = False
-    if not ok:
+        print('Message may not be empty')
+    if not subject or not message:
         return
 
     r = Reddit('{} Network Mailer v0.1 /u/{}'.format(settings.NETWORK_NAME, settings.USERNAME))
